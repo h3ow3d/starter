bbd67c71bdb2ef2cdcda6a6de37bc22e65834e32
h3ow3d <holdens.uk@proton.me>
2024-11-11 20:25:45 +0000

    dracula theme

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Load Dracula theme for LazyVim
+return {
+  {
+    "dracula/vim",
+    name = "dracula",
+    config = function()
+      vim.cmd("colorscheme dracula")
+    end,
+  },
+}
